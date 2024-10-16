36234d780d0d7d8512f3c45a8ff0afa7dd988d31
emmaculate2exceel <53976183+emmaculate-exceel@users.noreply.github.com>
2024-10-16 10:51:23 +0100

    Update script.js

--- web_development/script.js
+++ web_development/script.js
@@ -1,1 +1,5 @@
+//// write a javascript code that can perform a todo function 
+/// if the check box is not clicked user shouldn't be able to fix proceed 
+/// 
+
 document.getElementById("form").addEventListener(click,);
